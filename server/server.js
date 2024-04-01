cf77831cd93f085d701afdb18f3fbd71d62d0b2d
kimtokki <jjangnarana@gmail.com>
2024-04-02 02:35:50 +0900

    supabase api통신 구현중, frontend api 연동작업중

--- server/server.js
+++ server/server.js
@@ -13,6 +13,17 @@ const port = 3002;
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
+
+require('dotenv').config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function refreshhistory() {
+  let { data: page, error } = await supabase.from('about').select('*');
+}
 
 if (!fs.existsSync(directoryPath)) {
   fs.mkdirSync(directoryPath, { recursive: true });
@@ -41,6 +52,9 @@ app.use(
     },
   })
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/convert', upload.array('files'), async (request, response) => {
   const files = request.files;
@@ -123,7 +137,37 @@ app.get('/api/auth/status', (request, response) => {
 });
 app.get('/logout', (request, response) => {
   request.session.destroy();
+
   response.json({ message: 'Logged out' });
+});
+
+app.get('/about', async (request, response) => {
+  const { data, error } = await supabase.from('about').select('*');
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  response.json(data);
+});
+
+app.post('/about/update', async (request, response) => {
+  const { name, birthday, phone, email, introduction } = request.body;
+  console.log(introduction);
+  const { data, error } = await supabase
+    .from('about')
+    .update({
+      name: name,
+      birthday: birthday,
+      phone: phone,
+      email: email,
+      introduction: introduction,
+    })
+    .match({ id: 1 });
+
+  if (error) {
+    console.log(error);
+    return response.status(500).send('서버 오류 발생');
+  }
+  return response.status(200).send(data);
 });
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
